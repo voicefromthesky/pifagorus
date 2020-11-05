ebbf6a7fb261c4002e392e6b55f3fe2d79878644
voicefromthesky <andrewdoverin@gmail.com>
2020-11-05 21:53:04 +0200

    perfectus anus pifagorus

--- пифагор блеать/Program.cs
+++ пифагор блеать/Program.cs
@@ -28,29 +28,33 @@ namespace пифагор_блеать
         {
 
 
-            Console.WriteLine("Lenght?");
-            
+            Console.WriteLine("Lenght from?");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lenght to?");
             int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("Good, height?");
+            Console.WriteLine("Good, height from?");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("To?");
             int y = int.Parse(Console.ReadLine());
 
             int count = 1;
             int xy = x * y;
             int LocalSpaceCount1 = 0;
             int LocalSpace1 = x;
-      
-            for(; LocalSpace1 >= 1; LocalSpace1 /= 10) { LocalSpaceCount1++; }
+            int LocalSpaceY = y;
+            int LocalSpaceCountY = 0;
+            
+            for (; LocalSpaceY >= 1; LocalSpaceY /= 10) { LocalSpaceCountY++; }
+            for (; LocalSpace1 >= 1; LocalSpace1 /= 10) { LocalSpaceCount1++; }
             for (; xy >= 1; count++, xy /= 10) ;
-            for (int fuck = 0; fuck <= LocalSpaceCount1; fuck++) { Console.Write(" "); }
-           
-            for (int lenghth = 1; lenghth <= x; lenghth++)
+            for (int fuck = 0; fuck < LocalSpaceCount1; fuck++) { Console.Write(" "); }
+            Console.Write("|");
+            for (int lenghth = x1 ; lenghth <= x; lenghth++)
             {
                 
                
                 int LocalCount = 0;
                 int LocalShit = lenghth;
-                
-
                 for (; LocalShit >= 1; LocalShit /= 10) { LocalCount++; }
                 for (; LocalCount < count; LocalCount++) { Console.Write(" "); }
                 Console.Write(lenghth);
@@ -61,25 +65,33 @@ namespace пифагор_блеать
 
 
 
-            for (int i = 1; i <= y; i++)
+            for (int i = y1; i <= y; i++)
             {
-                if (i == 1) { for (int k = 0; k <= count* x+LocalSpaceCount1; k++) { Console.Write("_"); } Console.WriteLine(); }
+                
+               if (i == y1) 
+                {
+                    int k = 0;
+                    for (; k < LocalSpaceCount1; k++) { Console.Write("-"); }
+                    Console.Write("+");
+                    for (; k <= count * (x - x1) + LocalSpaceCount1 + count - 1; k++) { Console.Write("-"); }
 
-                for (int j = 1; j <= x; j++)
+
+                    Console.WriteLine(); 
+                }
+
+                
+                for (int j = x1; j <= x; j++)
                 {
 
-                    if (j == 1)
+                    if (j == x1)
                     {
-                        Console.Write(i);
                         int LocalCount1 = 0;
                         int LocalShit1 = i;
-                        int LocalSpace = x;
-                        int LocalSpaceCount = 0;
+                        
+                        for (; LocalShit1 >= 1; LocalShit1 /= 10) { LocalCount1++; }                       
+                        for (; LocalCount1 < LocalSpaceCountY; LocalCount1++) { Console.Write(" "); }
 
-                        for (; LocalShit1 >= 1; LocalShit1 /= 10) { LocalCount1++; }
-                        for (; LocalSpace >= 1; LocalSpace /= 10) { LocalSpaceCount++; }
-                        for (; LocalCount1 < LocalSpaceCount; LocalCount1++) { Console.Write(" "); }
-
+                        Console.Write(i);
                         Console.Write("|");
                     }
                     int LocalShit = i * j;
